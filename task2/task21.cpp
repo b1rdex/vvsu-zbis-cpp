a181dc9ec8943700f8f64d8f2dde4302f8035275
Anatoly Pashin <anatoly.pashin@gmail.com>
2018-03-06 00:24:08 +1000

    add tasks definition texts

--- task2/task21.cpp
+++ task2/task21.cpp
@@ -6,13 +6,15 @@
 #include "task21.h"
 
 void task21() {
+    // Ввести с консоли строку символов произвольной длины (до 80 символов).
+    // Поменять в ней все маленькие символы ‘a’ и ‘b’ на заглавные и вывести на консоль полученную строку.
+
     std::string input, result;
     std::cout << "enter string: ";
     std::cin >> input;
 
     for (auto &&letter : input) {
-        switch (letter)
-        {
+        switch (letter) {
             case 'a':
                 result += 'A';
                 break;
